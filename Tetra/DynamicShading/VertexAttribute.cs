83d3e26f2ff63393a2fe4f1be70416848c650a8f
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2017-01-27 21:43:25 +0100

    c# Vertex custom attribute to set type and size

---
+++ Tetra/DynamicShading/VertexAttribute.cs
@@ -0,0 +1,39 @@
+﻿//
+//  VertexAttribute.cs
+//
+//  Author:
+//       Jean-Philippe Bruyère <jp.bruyere@hotmail.com>
+//
+//  Copyright (c) 2017 jp
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using OpenTK.Graphics.OpenGL;
+
+namespace Tetra
+{
+	public class VertexAttribute : Attribute
+	{
+		public VertexAttribPointerType PointerType;
+		public int NbComponents;
+		public bool Normalized;
+		public VertexAttribute (int nbComponents, VertexAttribPointerType pointerType, bool normalized = false)
+		{
+			NbComponents = nbComponents;
+			PointerType = pointerType;
+			Normalized = normalized;
+		}
+	}
+}
+
